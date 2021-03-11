48c5ffbcc7487c0fafd3fa801cfbec0358d29926
Darryl Mangibin <darrylmangibin@gmail.com>
2021-03-11 13:21:05 +0800

    Remier on Event types

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,8 +1,15 @@
+import React, { useState } from 'react';
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('');
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  }
+
   return (
     <div>
-      <form>
-        <input />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={e => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>
