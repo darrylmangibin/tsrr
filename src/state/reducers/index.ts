6053fe5827e4c6f23aa62f9bb17a0a595127bffd
Darryl Mangibin <darrylmangibin@gmail.com>
2021-03-11 13:09:37 +0800

    Redux setup

---
+++ src/state/reducers/index.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import repositoriesReducer from './repositoriesReducer';
+
+const reducers = combineReducers({
+	respositories: repositoriesReducer,
+});
+
+export default reducers;
